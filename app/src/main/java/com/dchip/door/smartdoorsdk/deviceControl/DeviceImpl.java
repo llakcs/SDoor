6a6cfe86a0430357be9153ccaa306e0277517d79
llakcs <llacs@163.com>
2018-02-22 17:28:06 +0800

    commit

--- app/src/main/java/com/dchip/door/smartdoorsdk/deviceControl/DeviceImpl.java
+++ app/src/main/java/com/dchip/door/smartdoorsdk/deviceControl/DeviceImpl.java
@@ -194,6 +194,11 @@ public class DeviceImpl implements DeviceManager {
         if(mlogStrListner != null) {
             LogUtil.e(TAG, "###showmsg");
             mlogStrListner.resultStr(tag, msg);
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
